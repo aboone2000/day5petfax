0bbb7fe35a87aa6a81848766160d6ae35c683fab
aboone2000 <aboone2000@gmail.com>
2022-10-20 17:32:44 -0400

    Updated

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -14,9 +14,7 @@ def create_app():
     from . import pet 
     app.register_blueprint(pet.bp)
 
-    # register fact blueprint 
-    from . import fact
-    app.register_blueprint(fact.bp)
+    
 
     # return the app 
     return app
